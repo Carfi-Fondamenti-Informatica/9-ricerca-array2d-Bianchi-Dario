0140c71abb6687aad8c1730633bc2f776f44a00f
Bianchi-Dario <91790218+Bianchi-Dario@users.noreply.github.com>
2021-12-11 19:02:17 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,10 +1,1 @@
-//
-// Created by bianc on 11/12/2021.
-//
 
-#ifndef UNTITLED11_LIB_H
-#define UNTITLED11_LIB_H
-
-bool ricerca(char lista [10][20],char nome[1][20], int &pos)
-
-#endif //UNTITLED11_LIB_H
