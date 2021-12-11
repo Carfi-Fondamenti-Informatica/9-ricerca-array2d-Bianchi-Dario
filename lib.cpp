41963a41133c8631d8edb2d17162ed5f3682c38f
Bianchi-Dario <91790218+Bianchi-Dario@users.noreply.github.com>
2021-12-11 14:12:04 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -4,13 +4,13 @@
 #include "lib.h"
 #include <cstring>
 
-bool ricerca (char lista [10][20], char nome [1][20], int &pos){
-    for (int i=0; i<10; i++){
-        if (strcmp (lista [i], nome [0])==0){
-            pos= i;
+bool ricerca(char lista [10][20], char nome[1][20], int &pos){
+    for (int i = 0; i < 10; ++i) {
+        if(strcmp(lista [1], nome[0])==0){
+            pos = i;
             return true;
         }
+
     }
     return false;
 }
-
